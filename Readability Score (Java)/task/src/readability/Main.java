2016bcdcecbb738a8efb2a09316315e4156fb3c1
Max Pestov <pestov.maxik@gmail.com>
2024-02-07 19:15:14 +0400

    The first stage completed

--- Readability Score (Java)/task/src/readability/Main.java
+++ Readability Score (Java)/task/src/readability/Main.java
@@ -1,7 +1,17 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        try (var scanner = new Scanner(System.in)) {
+            String stringToEvaluate = scanner.nextLine();
+
+            if (stringToEvaluate.length() > 100) {
+                System.out.print("HARD");
+            } else {
+                System.out.println("EASY");
+            }
+        }
     }
 }
