db341035113352bba14ed333000381bc2e91173d
Max Pestov <pestov.maxik@gmail.com>
2024-02-07 22:25:21 +0400

    The second stage completed

--- Readability Score (Java)/task/src/readability/Main.java
+++ Readability Score (Java)/task/src/readability/Main.java
@@ -6,9 +6,17 @@ public class Main {
     public static void main(String[] args) {
         try (var scanner = new Scanner(System.in)) {
             String stringToEvaluate = scanner.nextLine();
+            String[] words = stringToEvaluate.split(" ");
+            int numberOfSentences = 0;
 
-            if (stringToEvaluate.length() > 100) {
-                System.out.print("HARD");
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].matches("\\w+[.!?]") || i == words.length - 1) {
+                    numberOfSentences++;
+                }
+            }
+
+            if ((double) words.length / numberOfSentences > 10) {
+                System.out.println("HARD");
             } else {
                 System.out.println("EASY");
             }
